8dfa567eeb705f1ad90055ec48725f63705f7328
towelie82 <31874531+towelie82@users.noreply.github.com>
2019-10-12 11:11:29 +0800

    Creation of some initial Fragment layouts and Home layout

---
+++ app/src/main/java/com/murdoch/ict376/whatsfordinner/Calendar.java
@@ -0,0 +1,36 @@
+package com.murdoch.ict376.whatsfordinner;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Calendar extends Fragment {
+
+    private CalendarViewModel mViewModel;
+
+    public static Calendar newInstance() {
+        return new Calendar();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.calendar_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(CalendarViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/murdoch/ict376/whatsfordinner/CalendarViewModel.java
@@ -0,0 +1,7 @@
+package com.murdoch.ict376.whatsfordinner;
+
+import androidx.lifecycle.ViewModel;
+
+public class CalendarViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
