1e30fd2594e340ef4694cb5189d9dec19780ae6d
towelie82 <31874531+towelie82@users.noreply.github.com>
2019-10-24 21:44:58 +0800

    Calendar Update: I've run through some of the sample code from prolificinteractive and have implemented a basic decorator. The decorator adds a red dot on some of the days and can be configured for our days with meals.

--- app/src/main/java/com/murdoch/ict376/whatsfordinner/Calendar.java
+++ app/src/main/java/com/murdoch/ict376/whatsfordinner/Calendar.java
@@ -1,46 +1,84 @@
 package com.murdoch.ict376.whatsfordinner;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 
 import android.widget.Toast;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.murdoch.ict376.whatsfordinner.decorators.MealDecorator;
 
-import com.murdoch.ict376.whatsfordinner.view.CalendarViewModel;
-import com.murdoch.ict376.whatsfordinner.view.RecipeListAdapter;
-import com.murdoch.ict376.whatsfordinner.view.RecipeViewModel;
-import com.murdoch.ict376.whatsfordinner.view.RecyclerViewClickListener;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Month;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
 
-public class Calendar extends AppCompatActivity {
+public class Calendar extends AppCompatActivity implements OnDateSelectedListener {
 
-    private CalendarViewModel mCalendarViewModel;
     private MaterialCalendarView mealCalendar;
-    //private ArrayList<Date> markedDates;
 
-
-    //RecipeListAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_fragment);
-
-
-        setContentView(R.layout.calendar_fragment);
         mealCalendar = (MaterialCalendarView) findViewById(R.id.mealCalendar); // get the reference of CalendarView
-        /*mealCalendar.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
-        mealCalendar.setUnfocusedMonthDateColor(Color.BLUE); // set the yellow color for the dates of an unfocused month
-        mealCalendar.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
-        mealCalendar.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
-        // perform setOnDateChangeListener event on CalendarView
-        mealCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                // display the selected date by using a toast
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
-            }
-        });
-        */
+
+        mealCalendar.setOnDateChangedListener(this);
+        mealCalendar.setShowOtherDates(MaterialCalendarView.SHOW_ALL);
+
+        final LocalDate instance = LocalDate.now();
+        mealCalendar.setSelectedDate(instance);
+
+        new ApiSimulator().executeOnExecutor(Executors.newSingleThreadExecutor());
+
     }
+
+    @Override
+    public void onDateSelected(
+            @NonNull MaterialCalendarView mealCalendar,
+            @NonNull CalendarDay date,
+            boolean selected) {
+
+        //oneDayDecorator.setDate(date.getDate());
+        mealCalendar.invalidateDecorators();
+    }
+
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LocalDate temp = LocalDate.now().minusMonths(2);
+            final ArrayList<CalendarDay> dates = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                final CalendarDay day = CalendarDay.from(temp);
+                dates.add(day);
+                temp = temp.plusDays(5);
+            }
+
+            return dates;
+        }
+
+    @Override
+    protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+        super.onPostExecute(calendarDays);
+
+        if (isFinishing()) {
+            return;
+        }
+
+        mealCalendar.addDecorator(new MealDecorator(Color.RED, calendarDays));
+    }
+}
 }
