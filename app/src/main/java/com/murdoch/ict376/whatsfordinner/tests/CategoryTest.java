df27ce345d9cba4eba298f1be7b5acf6ef437075
towelie82 <31874531+towelie82@users.noreply.github.com>
2019-11-01 22:23:40 +0800

    Test: Simple unit test

---
+++ app/src/main/java/com/murdoch/ict376/whatsfordinner/tests/CategoryTest.java
@@ -0,0 +1,12 @@
+package com.murdoch.ict376.whatsfordinner.tests;
+
+public class CategoryTest {
+
+    public static boolean isValidName(String str){
+
+        if (str == null || str == "")
+            return false;
+        else
+            return true;
+    }
+}
---
+++ app/src/test/java/com/murdoch/ict376/whatsfordinner/CategoryTests.java
@@ -0,0 +1,27 @@
+package com.murdoch.ict376.whatsfordinner;
+
+import com.murdoch.ict376.whatsfordinner.tests.CategoryTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CategoryTests {
+
+    @Test
+    public void nameValidator_isCorrectName() {
+        assertTrue(CategoryTest.isValidName("Chicken"));
+
+    }
+
+    @Test
+    public void nameValidator_isEmptyString() {
+        assertFalse(CategoryTest.isValidName(""));
+
+    }
+
+    @Test
+    public void nameValidator_isNull() {
+        assertFalse(CategoryTest.isValidName(null));
+
+    }
+}
